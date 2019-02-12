6ce5ff70c15611c352d61d89ea9977987aae3707
caoshengming <caoshengming@10.0.0.12>
2019-02-12 19:01:25 +0800

    add old scripts

---
+++ basic/mkdir.sh
@@ -0,0 +1,2 @@
+# -p make recursive file directory; {} contain same level elements
+mkdir -p testmkdir/{tmpfile1,tmpfile2,tmpfile3}
---
+++ basic/tree.sh
@@ -0,0 +1,5 @@
+tree -d       #show directory
+tree -df      #show full path
+tree -dL 2    #show with layer 2
+tree -L 1 -F  #show with layer 1 and show symbol "/"
+tree -dfi     #not show tree-like directory
